8e6bce12bb81afbdb0e171da4ca91249f050db2c
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2023-11-30 20:56:36 +0600

    cookie fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const signToken = (id) => {
 const cookieOptions = {
   httpOnly: true,
   path: "/",
-  secure: process.env.NODE_ENV === "production",
+  secure: true,
   sameSite: "None",
   maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000, // 90 days
 };
